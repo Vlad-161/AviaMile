f6c3da1d1fde08f15a8b48c11f17beb14b8f35bb
Vlad Polovinkin <polovinkinvlaaad@gmail.com>
2025-01-24 19:33:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int TicketPrice = 15300;
+        int OneMile = 20;
+
+      System.out.println("Bonus:" + (TicketPrice / OneMile));
+    }
+}
